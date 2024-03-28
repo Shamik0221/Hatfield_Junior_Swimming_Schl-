4777ae8cec949ab9e7d0c16d9fcf24e2c1f0fbc5
Shamik0221 <shamikvijaynstudy@gmail.com>
2024-03-28 18:49:12 +0530

    Update all setters and getters methods of the Coach Class.

--- com/hjss/swimmingschool/Coach.java
+++ com/hjss/swimmingschool/Coach.java
@@ -9,7 +9,6 @@ public class Coach {
     private String phone;
     private int yearOfExperience;
 
-
     public Coach(String name, String gender, int age, String phone,int yearOfExperience) {
         this.id =  sequence++;
         this.name = name;
@@ -17,5 +16,64 @@ public class Coach {
         this.age = age;
         this.phone = phone;
         this.yearOfExperience = yearOfExperience;
+    }
+
+    // Setters Method of the Coach Class
+    
+    public void setName(String newName)   {
+        this.name = newName;
+    }
+
+    public void setGender(String newGender) {
+        this.gender = newGender;
+    }
+
+    public void setAge(int newAge) {
+        this.age = newAge;
+    }
+
+    public void setPhone(String newPhone) {
+        this.phone = newPhone;
+    }
+
+    public void setYearOfExperience(int years) {
+        this.yearOfExperience = years;
+    }
+    
+    // Getter Methods of the Coach Class
+    public String getName()   {
+        return this.name;
+    }
+
+    public String getGender( ) {
+        return this.gender ;
+    }
+
+    public int  getAge( ) {
+        return this.age; 
+    }
+
+    public String  getPhone() {
+        return this.phone ; 
+    }
+
+    public int getYearOfExperience() {
+        return this.yearOfExperience ; 
+    }
+
+    
+    @Override
+    public String toString() {
+        return name + ", " + age + "," + gender + ",(" + phone + ") " + yearOfExperience;
+    }
+    
+
+    public void printInfo() {
+        System.out.println("Coach Id       : " + id);
+        System.out.println("Coach Name     : " + name);
+        System.out.println("Coach Gender   : " + gender);
+        System.out.println("Coach Age      : " + age);
+        System.out.println("Coach Phone    : " + phone);
+        System.out.println("Coach Exp      : " + yearOfExperience);
     }
 }
--- com/hjss/swimmingschool/Learner.java
+++ com/hjss/swimmingschool/Learner.java
@@ -32,4 +32,6 @@ public class Learner {
     public void updateSession(){
 
     }
+
+
 }
