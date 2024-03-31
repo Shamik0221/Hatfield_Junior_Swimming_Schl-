4622971b49e3f58b5cda8ccfcaad3f98bc9d3320
Shamik0221 <shamikvijaynstudy@gmail.com>
2024-03-31 09:08:40 +0530

    Update all setters and getters methods, addReview and addLearner  methods in Session Class/

--- com/hjss/swimmingschool/Session.java
+++ com/hjss/swimmingschool/Session.java
@@ -11,23 +11,65 @@ public class Session {
     private String day;
     private String timeslot;
     private String coachName;
-    private Review sessionReview;
 
-    private int currentLearner;
+    private int numberLearners;
 
+    private ArrayList<Review> listofReviews;
     private ArrayList<Learner> listOfLearners ;
 
+
+    // Constructor of the class
     public Session(String day, String timeslot, String coachName) {
-        this.id = sequence++; 
+        this.id = sequence++;
         this.day = day;
         this.timeslot = timeslot;
         this.coachName = coachName;
-        this.currentLearner = 0;
+        this.numberLearners = 0;
         this.listOfLearners = new ArrayList<Learner>();
+        this.listofReviews = new ArrayList<Review>();
     }
 
-    public void updateSession() {
+    // Setter methods for Session Class
+    public void setDay(String day) {
+        this.day = day;
+    }
 
+    public void setTimeSlot(String timeslot){
+        this.timeslot = timeslot;
+    }
+
+    public void setCoach(string coach){
+        this.coachName = coach;
+    }
+
+    // Getter Methods for Session
+    public String getDay() {
+        return this.day;
+    }
+
+    public String getTimeSlot(){
+        return this.timeslot;
+    }
+
+    public String getCoach(){
+        return this.coachName;
+    }
+
+
+    // Adding Review into the list
+    public void addReview(Review r){
+        listofReviews.add(r);
+    }
+
+    // Adding Learners into the session
+    public boolean addLearner(Learner l) {
+        boolean status = false;
+        if (numberLearners < maxLearner) {
+            listOfLearners.add(l);
+            numberLearners++;
+            status = true;
+        }
+        return status;
     }
 
     public void printInfo() {
